9db97f2717183c2f9b58eb873cd6a575a92eb28f
Christopher Hindson <chris_hindson@hotmail.com>
2024-08-09 17:41:30 +0100

    Initial commit

---
+++ src/main/java/com/example/skillsassignment/SkillsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillsassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsAssignmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SkillsAssignmentApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/skillsassignment/SkillsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillsassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsAssignmentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
